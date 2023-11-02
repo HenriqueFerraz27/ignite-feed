a7e349b0aecc882364deea733e5653666b9d2562
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-11-02 12:20:56 -0700

    🏗feat: iterating posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,32 @@ import "./styles/App.css";
 import { Header, Profile, Post } from "./components";
 
 function App() {
+  const posts = [
+    {
+      author: {
+        avatarUrl: "https://github.com/HenriqueFerraz27.png",
+        name: "Henrique Ferraz",
+        role: "Web Developer",
+      },
+      content: `Fala galeraa 👋
+
+      Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀`,
+      puplished: new Date("2023-11-02 12:09:00"),
+    },
+    {
+      author: {
+        avatarUrl: "https://github.com/HenriqueFerraz27.png",
+        name: "Luiz Ferraz",
+        role: "Web Developer",
+      },
+      content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vero
+      debitis repellendus repudiandae quibusdam ex quis nobis atque eaque hic
+      dolorum sit tempora, rem sed praesentium quisquam accusamus iure
+      eveniet.`,
+      puplished: new Date("2023-12-02 12:09:00"),
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -12,7 +38,9 @@ function App() {
         </aside>
 
         <article>
-          <Post />
+          {posts.map((post, index) => {
+            return <Post key={index} />;
+          })}
         </article>
       </main>
     </>
